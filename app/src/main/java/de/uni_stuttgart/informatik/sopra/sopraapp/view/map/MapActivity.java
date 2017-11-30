258d82d1b5dbf6c2c73b7cdb6a898788ac0f8a27
Andrew Almaguer <drewbudd@gmail.com>
2017-11-30 14:21:54 +0100

    Add manage package and new fragments
    Also moved ManageServiceFragment to the manage package

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/view/map/MapActivity.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/view/map/MapActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import de.uni_stuttgart.informatik.sopra.sopraapp.R;
+import de.uni_stuttgart.informatik.sopra.sopraapp.view.manage.ContractsFragment;
+import de.uni_stuttgart.informatik.sopra.sopraapp.view.manage.DamagesFragment;
+import de.uni_stuttgart.informatik.sopra.sopraapp.view.manage.FieldsFragment;
+import de.uni_stuttgart.informatik.sopra.sopraapp.view.manage.ManageServiceFragment;
+import de.uni_stuttgart.informatik.sopra.sopraapp.view.manage.SearchFragment;
 
 public class MapActivity extends AppCompatActivity implements
-        MapFragment.OnFragmentInteractionListener, ManageServiceFragment.OnFragmentInteractionListener {
+        MapFragment.OnFragmentInteractionListener, ManageServiceFragment.OnFragmentInteractionListener, ContractsFragment.OnFragmentInteractionListener, DamagesFragment.OnFragmentInteractionListener, FieldsFragment.OnFragmentInteractionListener, SearchFragment.OnFragmentInteractionListener {
 
 
     @Override
