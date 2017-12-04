b13dc3dd856d895b4ac4a29d1049dd02aed6ff56
Andrew Almaguer <drewbudd@gmail.com>
2017-12-04 16:21:46 +0100

    Add empty files for add damage fragment

---
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/view/fragments/AddDamageFragment.java
@@ -0,0 +1,10 @@
+package de.uni_stuttgart.informatik.sopra.sopraapp.view.fragments;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by Andrew on 12/4/2017.
+ */
+
+public class AddDamageFragment extends DialogFragment {
+}
