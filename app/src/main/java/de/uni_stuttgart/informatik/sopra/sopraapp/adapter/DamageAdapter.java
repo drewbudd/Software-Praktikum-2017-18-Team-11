6a3b6260cbfd68d5633c02c32a103349b850328c
Stefan Zindl <st148777@stud.uni-stuttgart.de>
2018-01-08 21:33:35 +0100

    started critical feature: delete Damage todo change to damage in list view

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/adapter/DamageAdapter.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/adapter/DamageAdapter.java
@@ -6,78 +6,106 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import de.uni_stuttgart.informatik.sopra.sopraapp.R;
-import de.uni_stuttgart.informatik.sopra.sopraapp.model.damage.Damage;
+import de.uni_stuttgart.informatik.sopra.sopraapp.model.fields.Field;
 import de.uni_stuttgart.informatik.sopra.sopraapp.services.UserService;
+import de.uni_stuttgart.informatik.sopra.sopraapp.view.map.MapActivity;
 
 /**
  * @author Stefan Zindl
  * @since 2017/11/15
- *
- *
- * list of Damages
+ * <p>
+ * <p>
+ * Adapter for all fields
  * https://github.com/codepath/android_guides/wiki/Using-the-RecyclerView
- *
  */
 
 public class DamageAdapter extends RecyclerView.Adapter<DamageAdapter.ViewHolder> {
 
 
-    private List<Damage> damages;
+    private List<Field> fieldList;
+    private List<Field> filterdList;
     private List<ViewHolder> views = new ArrayList<>();
     private Context context;
 
-    /**
-     * constructor
-     * @param context
-     * @param events
-     */
-    public DamageAdapter(Context context, List<Damage> events) {
-        this.damages = events;
+    public DamageAdapter(Context context, List<Field> events) {
+        this.fieldList = events;
         this.context = context;
+        filterdList = events;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.card_damageevent, parent, false);
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_field, parent, false);
 
-        ViewHolder vh = new ViewHolder(view);
+        ViewHolder vh = new ViewHolder(v);
         views.add(vh);
+
         return vh;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Damage event = damages.get(position);
+    public void onBindViewHolder(DamageAdapter.ViewHolder holder, int position) {
+        Field event = fieldList.get(position);
         String insuranceHolder;
 
-        holder.associateDamage = event;
+        holder.associatedField = event;
 
-        holder.inscuredPerson.setText(UserService.getInstance().getCurrentUser().getName());
+        if (event.getFieldType() != null) {
+
+            holder.fieldType.setText(event.getFieldType());
+        }
+        if (event.getOwner() != null) {
+            holder.inscuredPerson.setText(event.getOwner().getName());
+        }
+        if (event.getGutachter() != null) {
+            holder.gutachter.setText(event.getGutachter().getName());
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return this.damages.size();
+        return fieldList.size();
     }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public CardView card;
         public TextView inscuredPerson;
-        public Damage associateDamage;
+        public Field associatedField;
+        public TextView fieldType;
+        public TextView gutachter;
+        public ImageButton deleteFieldButton;
 
         public ViewHolder(View itemView) {
             super(itemView);
             card = (CardView) itemView;
             inscuredPerson = itemView.findViewById(R.id.insuredPerson);
+            fieldType = itemView.findViewById(R.id.card_field_type);
+            gutachter = itemView.findViewById(R.id.gutachter);
+            deleteFieldButton = itemView.findViewById(R.id.deleteFieldButton);
+            deleteFieldButton.setVisibility(View.GONE);
+            if(UserService.getInstance().getCurrentUser().isGutachter()) {
+                deleteFieldButton.setVisibility(View.VISIBLE);
+                deleteFieldButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MapActivity.dataService.deleteFieldById(getAdapterPosition());
+                        notifyItemChanged(getAdapterPosition());
+                        notifyItemChanged(getAdapterPosition(), fieldList.size());
+                        notifyDataSetChanged();
+                    }
+                });
+            }
         }
     }
 }
--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/view/manage/DamagesFragment.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/view/manage/DamagesFragment.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.uni_stuttgart.informatik.sopra.sopraapp.R;
+import de.uni_stuttgart.informatik.sopra.sopraapp.adapter.FieldListAdapter;
+import de.uni_stuttgart.informatik.sopra.sopraapp.model.fields.Field;
+import de.uni_stuttgart.informatik.sopra.sopraapp.view.map.MapActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +35,12 @@ public class DamagesFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private View rootView;
+    private RecyclerView recycler;
+    private List<Field> fields = new ArrayList<>();
+    private FieldListAdapter adapter;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -65,7 +79,14 @@ public class DamagesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_damages, container, false);
+        rootView = inflater.inflate(R.layout.fragment_fields, container, false);
+        recycler = rootView.findViewById(R.id.recycler_fieldsview);
+        fields = new ArrayList<>();
+        fields = MapActivity.dataService.getFields();
+        adapter = new FieldListAdapter(getContext(), fields);
+        recycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recycler.setAdapter(adapter);
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
