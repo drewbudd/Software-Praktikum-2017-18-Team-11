a4abce32f3e6a6d6ce50294842b1e0c6bbf9c2d1
Aimn Ahmed <st150637@stud.uni-stuttgart.de>
2018-01-28 20:16:01 +0100

    update settingsActivity

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/view/map/MapSettings.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/view/map/MapSettings.java
@@ -1,31 +1,80 @@
 package de.uni_stuttgart.informatik.sopra.sopraapp.view.map;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Handler;
+import android.os.Looper;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.List;
+import java.util.Locale;
 
 import de.uni_stuttgart.informatik.sopra.sopraapp.R;
 
-public class MapSettings extends ListActivity {
-    private String[] entries = {"Logout", "Change Language"};
+public class MapSettings extends AppCompatActivity {
+    private String[] entries;
     ListView l;
+    private Spinner changeLang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_settings);
-        l = getListView();
-        ArrayAdapter<String> stringAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, entries);
-        l.setAdapter(stringAdapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+
+    /**
+     * updates the current language to the selected language
+     *
+     * @param languageToLoad
+     */
+    private void setLanguage(String languageToLoad) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+
+
+        /*
+        Locale locale;
+        if(languageToLoad.equals("not-set")){ //use any value for default
+            locale = Locale.getDefault();
+        }
+        else {
+            locale = new Locale(languageToLoad);
+        }
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+                */
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putString("LANG", languageToLoad).commit();
+                Configuration config = getBaseContext().getResources().getConfiguration();
+                Locale locale = new Locale(languageToLoad);
+                Locale.setDefault(locale);
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                recreate();
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+
+            }
+        });
+
 
     }
 }
